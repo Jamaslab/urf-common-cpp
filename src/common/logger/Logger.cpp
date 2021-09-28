cf3a8ea2820027725a60b6c97ca8684314506568
Giacomo Lunghi <jamaslab@gmail.com>
2021-09-28 17:00:51 +0200

    Corrected interface of Logger which was causing to overwrite the existing object

--- src/common/logger/Logger.cpp
+++ src/common/logger/Logger.cpp
@@ -3,7 +3,7 @@
 namespace urf {
 namespace common {
 
-logger::logger& getLoggerInstance(const std::string component) {
+logger::logger getLoggerInstance(const std::string component) {
     std::vector<logger::sink_ptr> sinks;
     if (std::getenv(LOGGER_ENABLE_LOGGING)) {
         sinks.push_back(std::make_shared<logger::sinks::stdout_color_sink_mt>());
@@ -13,7 +13,7 @@ logger::logger& getLoggerInstance(const std::string component) {
         sinks.push_back(std::make_shared<logger::sinks::rotating_file_sink_mt>("logs/log.txt" , 1024 * 1024, 5, true));
     }
 
-    static logger::logger log(component, begin(sinks), end(sinks));
+    logger::logger log(component, begin(sinks), end(sinks));
     // auto log = std::make_unique<logger::logger>(component, begin(sinks), end(sinks));
 
     if (std::getenv(LOGGER_ENABLE_TRACE)) {
--- src/urf/common/logger/Logger.hpp
+++ src/urf/common/logger/Logger.hpp
@@ -21,7 +21,7 @@ namespace common {
 
 namespace logger = spdlog;
 
-URF_COMMON_EXPORT logger::logger& getLoggerInstance(const std::string component);
+URF_COMMON_EXPORT logger::logger getLoggerInstance(const std::string component);
 
 }  // namespace common
 }  // namespace urf
