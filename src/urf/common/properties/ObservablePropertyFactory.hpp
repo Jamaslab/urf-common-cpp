a20eb1144023759e0e22607054a9bfe23997cfc5
Giacomo Lunghi <jamaslab@gmail.com>
2021-10-25 10:13:18 +0000

    ObservableProperty factory from datatype and property type

---
+++ src/urf/common/properties/ObservablePropertyFactory.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#if defined(_WIN32) || defined(_WIN64)
+#    include "urf/common/urf_common_export.h"
+#else
+#    define URF_COMMON_EXPORT
+#endif
+
+#include "urf/common/properties/ObservableProperty.hpp"
+
+#include <map>
+#include <tuple>
+#include <typeinfo>
+
+namespace urf {
+namespace common {
+namespace properties {
+
+class ObservablePropertyFactory {
+    static std::map<std::string, std::map<PropertyType, std::function<IObservableProperty*()>>>
+        registered_;
+
+    ObservablePropertyFactory() = default;
+
+ public:
+    static std::shared_ptr<IObservableProperty> create(const std::string& datatype,
+                                                       const PropertyType& type);
+    static bool
+        registerDatatype(const std::string& datatype,
+                     std::map<PropertyType, std::function<IObservableProperty*()>> constructors);
+};
+
+} // namespace properties
+} // namespace common
+} // namespace urf
+
+#define REGISTER_PROPERTY_DATATYPE(name, datatype)                          \
+namespace _urf_##datatype {                                                 \
+    bool tmp = urf::common::properties::ObservablePropertyFactory::registerDatatype(                                                   \
+        name,                                                                                 \
+        {{PropertyType::Property, []() { return new urf::common::properties::ObservableProperty<datatype>(); }},                           \
+         {PropertyType::Setting, []() { return new urf::common::properties::ObservableSetting<datatype>(); }},                             \
+         {PropertyType::RangeSetting, []() { return new urf::common::properties::ObservableSettingRanged<datatype>(); }},                 \
+         {PropertyType::ListSetting, []() { return new urf::common::properties::ObservableSettingList<datatype>(); }}});                  \
+}                                                                                                                              \
+\
+template<> \
+std::string urf::common::properties::getTemplateDatatype<datatype>::operator()() { \
+    return name; \
+}
+
+#define REGISTER_PROPERTY_DATATYPE_VECTOR(name, datatype)                          \
+namespace _urf_##datatype {                                                 \
+    bool tmp_vec = urf::common::properties::ObservablePropertyFactory::registerDatatype(                                                   \
+        "vector/"+std::string(name),                                                                                 \
+        {{PropertyType::Property, []() { return new urf::common::properties::ObservableProperty<std::vector<datatype>>(); }},                           \
+         {PropertyType::Setting, []() { return new urf::common::properties::ObservableSetting<std::vector<datatype>>(); }},                             \
+         {PropertyType::RangeSetting, []() { return new urf::common::properties::ObservableSettingRanged<std::vector<datatype>>(); }},                 \
+         {PropertyType::ListSetting, []() { return new urf::common::properties::ObservableSettingList<std::vector<datatype>>(); }}});                  \
+}
+
---
+++ tests/unittests/properties/ObservablePropertyFactoryTests.cpp
@@ -0,0 +1,28 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "urf/common/properties/ObservablePropertyFactory.hpp"
+
+using namespace urf::common::properties;
+
+TEST(ObservablePropertyFactoryShould, correctlyCreateDatatypes) {
+    auto prop = ObservablePropertyFactory::create("vector/float32", PropertyType::ListSetting);
+    ASSERT_EQ(prop->datatype(), "vector/float32");
+    ASSERT_EQ(prop->type(), PropertyType::ListSetting);
+
+    prop = ObservablePropertyFactory::create("float32", PropertyType::ListSetting);
+    ASSERT_EQ(prop->datatype(), "float32");
+    ASSERT_EQ(prop->type(), PropertyType::ListSetting);
+
+    prop = ObservablePropertyFactory::create("float64", PropertyType::RangeSetting);
+    ASSERT_EQ(prop->datatype(), "float64");
+    ASSERT_EQ(prop->type(), PropertyType::RangeSetting);
+
+    prop = ObservablePropertyFactory::create("bool", PropertyType::Setting);
+    ASSERT_EQ(prop->datatype(), "bool");
+    ASSERT_EQ(prop->type(), PropertyType::Setting);
+
+    prop = ObservablePropertyFactory::create("uint8", PropertyType::Property);
+    ASSERT_EQ(prop->datatype(), "uint8");
+    ASSERT_EQ(prop->type(), PropertyType::Property);
+}
